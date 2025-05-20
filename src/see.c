6942cba84b984276827ce1daba243f30101d5a2c
itlwas <itlwas@duck.com>
2025-05-20 21:58:19 +0300

    refactor: Use else-if for mutually exclusive option checks

--- src/see.c
+++ src/see.c
@@ -156,8 +156,7 @@ int main(int argc, char *argv[]) {
 			}
 			if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
 				usage(); /* Exits */
-			}
-			if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0) {
+			} else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0) {
 				version(); /* Exits */
 			}
 		}
